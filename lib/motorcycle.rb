0e7de48907289cb03489d663157a90af85a83ee9
Derek Kastner <dkastner@gmail.com>
2010-08-12 15:07:32 -0400

    Using emitter gem

--- lib/motorcycle.rb
+++ lib/motorcycle.rb
@@ -1,23 +1,10 @@
+require 'emitter'
+
 module BrighterPlanet
   module Motorcycle
-    extend self
-
-    def included(base)
-      require 'cohort_scope'
-      require 'falls_back_on'
-      require 'falls_back_on/active_record_ext'
-
-      require 'motorcycle/carbon_model'
-      require 'motorcycle/characterization'
-      require 'motorcycle/data'
-      require 'motorcycle/summarization'
-
-      base.send :include, BrighterPlanet::Motorcycle::CarbonModel
-      base.send :include, BrighterPlanet::Motorcycle::Characterization
-      base.send :include, BrighterPlanet::Motorcycle::Data
-      base.send :include, BrighterPlanet::Motorcycle::Summarization
-    end
-    def motorcycle_model
+    extend BrighterPlanet::Emitter
+    
+    def self.motorcycle_model
       if Object.const_defined? 'Motorcycle'
         ::Motorcycle
       elsif Object.const_defined? 'MotorcycleRecord'
