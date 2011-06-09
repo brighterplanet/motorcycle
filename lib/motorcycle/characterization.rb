da6f4bb86d92ab040203fc809ceb29f8a8544a6e
Seamus Abshere <seamus@abshere.net>
2011-06-09 00:26:41 -0500

    make tests green with new sniff

--- lib/motorcycle/characterization.rb
+++ lib/motorcycle/characterization.rb
@@ -3,9 +3,9 @@ module BrighterPlanet
     module Characterization
       def self.included(base)
         base.characterize do
-          has :fuel_efficiency, :measures => :length_per_volume
-          has :annual_distance_estimate, :measures => :length, :trumps => :weekly_distance_estimate
-          has :weekly_distance_estimate, :measures => :length, :trumps => :annual_distance_estimate
+          has :fuel_efficiency #, :measures => :length_per_volume
+          has :annual_distance_estimate, :measures => :length
+          has :weekly_distance_estimate, :measures => :length
           has :acquisition
           has :retirement
         end
--- lib/motorcycle/data.rb
+++ lib/motorcycle/data.rb
@@ -2,18 +2,14 @@ module BrighterPlanet
   module Motorcycle
     module Data
       def self.included(base)
-        base.data_miner do
-          schema do
-            string   'name'
-            date     'date' # ?
-            float    'fuel_efficiency'
-            float    'annual_distance_estimate'
-            float    'weekly_distance_estimate'
-            date     'acquisition'
-            date     'retirement'
-          end
-          
-          process :run_data_miner_on_belongs_to_associations
+        base.create_table do
+          string   'name'
+          date     'date' # ?
+          float    'fuel_efficiency'
+          float    'annual_distance_estimate'
+          float    'weekly_distance_estimate'
+          date     'acquisition'
+          date     'retirement'
         end
       end
     end
