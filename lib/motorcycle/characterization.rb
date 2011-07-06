03d289a0219291be75846313640423aac3df6167
Seamus Abshere <seamus@abshere.net>
2011-07-05 21:23:36 -0500

    recognize units

--- lib/motorcycle/characterization.rb
+++ lib/motorcycle/characterization.rb
@@ -3,9 +3,9 @@ module BrighterPlanet
     module Characterization
       def self.included(base)
         base.characterize do
-          has :fuel_efficiency #, :measures => :length_per_volume
-          has :annual_distance_estimate, :measures => :length
-          has :weekly_distance_estimate, :measures => :length
+          has :fuel_efficiency, :measures => Measurement::BigLengthPerVolume
+          has :annual_distance_estimate, :measures => Measurement::BigLength
+          has :weekly_distance_estimate, :measures => Measurement::BigLength
           has :acquisition
           has :retirement
         end
