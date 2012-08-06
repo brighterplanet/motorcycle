6f38fef57fcea842e2196fd556dfe95fa62c986c
Derek Kastner <dkastner@gmail.com>
2012-08-06 13:54:19 -0500

    Enumerate emitter requires

--- lib/motorcycle.rb
+++ lib/motorcycle.rb
@@ -1,5 +1,11 @@
 require 'emitter'
 
+require 'motorcycle/impact_model'
+require 'motorcycle/characterization'
+require 'motorcycle/data'
+require 'motorcycle/relationships'
+require 'motorcycle/summarization'
+
 module BrighterPlanet
   module Motorcycle
     extend BrighterPlanet::Emitter
